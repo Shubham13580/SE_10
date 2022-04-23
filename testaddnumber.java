77043092d396f0c1d792ae67f8ed8a40f1d27042
Shubham13580 <104272861+Shubham13580@users.noreply.github.com>
2022-04-23 23:28:12 +0530

    Add files via upload

---
+++ testaddnumber.java
@@ -0,0 +1,17 @@
+//E20CSE324_SHUBHAM
+package jUnitTestPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testaddnumber {
+
+	@Test
+	public void test() {
+		jUnitFunctions junit = new jUnitFunctions();
+		int result = junit.addNumber(100, 200);
+		assertEquals(300, result);
+	}
+
+}
